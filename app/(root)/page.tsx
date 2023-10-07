b66c56a9b0982a89b0cbabbab6a9ff54be383288
Farhan Asif <farhanmatics@gmail.com>
2023-10-07 11:35:23 +0100

    shadcn and page structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div className="flex justify-center h-full align-middle p-4">
+      <Button variant="outline">Click me</Button>
+    </div>
+    
+  )
+}
